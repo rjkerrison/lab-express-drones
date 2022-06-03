551a692b34c820121c4bc9b51861803bf9399920
Haroun B <haroun@harryb.dev>
2022-06-03 17:38:37 +0200

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const { Schema, model } = require(`mongoose`);
+
+const droneSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  propellers: {
+    type: Number,
+    min: 0,
+    required: true
+  }, 
+  maxSpeed: {
+    type: Number,
+    min: 0,
+    required: true
+  }
+});
+
+const Drone = model(`Drone`, droneSchema);
+
+module.exports = Drone;
