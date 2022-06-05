2118b2ef82dc714f428309910fb61aff17d4f911
caetanolmsouza <caetanomartins@hotmail.com>
2022-06-05 13:40:19 +0200

    first

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
 // Iteration #1
+
+const { Schema, model } = require('mongoose')
+// const mongoose = require('mongoose')
+
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+     maxSpeed: Number,
+  }
+)
+
+const Drones = model('Drones', droneSchema)
+
+module.exports = Drones
