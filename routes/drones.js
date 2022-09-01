a19634661dee049abe3c98895c763115cf5200d7
Guillaume <guillaume.chatriot@gmail.com>
2022-09-01 16:02:59 +0200

    do it by myself

--- routes/drones.js
+++ routes/drones.js
@@ -1,26 +1,54 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Drone = require("../models/Drone.model");
 
-// require the Drone model here
+router.get("/drones", async (req, res, next) => {
+  try {
+    const allDrones = await Drone.find(req.body);
+    res.json(allDrones);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
-})
+router.post("/drones", async (req, res, next) => {
+  try {
+    const { name, propellers, maxSpeed } = req.body;
+    const newDrone = await Drone.create({
+      name,
+      propellers,
+      maxSpeed,
+    });
+    res.status(201).json(newDrone);
+  } catch (error) {
+    return res.status(400).json({ message: "BAD REQUEST" });
+  }
+});
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-})
+router.post("/drones/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).json({ message: "NOT FOUND" });
+    }
+    const { name, propellers, maxSpeed } = req.body;
+    const updateDrone = await Drone.findByIdAndUpdate(
+      id,
+      {
+        name,
+        propellers,
+        maxSpeed,
+      },
+      { new: true }
+    );
+    res.status(200).json({ updateDrone });
+  } catch (error) {
+    return res.status(400).json({ message: "BAD REQUEST" });
+  }
+});
 
-router.post('/drones/:id', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-})
-
-router.delete('/drones/:id', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
-})
-
-module.exports = router
+router.delete("/drones/:id", async (req, res, next) => {
+  await Drone.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+module.exports = router;
